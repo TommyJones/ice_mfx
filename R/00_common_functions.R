3aae04911ff70db72f71d6223d22c68bd95ef3cf
Tommy Jones <jones.thos.w@gmail.com>
2017-11-05 20:48:54 -0500

    trying to add functionality for factor variables. It's wrong. Oh, so very wrong.

--- R/00_common_functions.R
+++ R/00_common_functions.R
@@ -11,7 +11,7 @@ CalcMfx <- function(object, X, pred_fun = predict, predictors = colnames(X),
                     max_pts = 100, ...){
   
   ### Check consistency of inputs ----
-  
+
   ### Prepare some global variables ----
 
   ### Do assuming 1 predictor and expand later ----
@@ -31,7 +31,11 @@ CalcMfx <- function(object, X, pred_fun = predict, predictors = colnames(X),
     p <- predictors
     
     # get the sequence to iterate over
-    if (is.na(max_pts)) {
+    if (is.factor(X[[ p ]])) {
+      
+      pts <- levels(X[[ p ]])
+      
+    } else if (is.na(max_pts)) {
       
       pts <- sort(unique(X[[ p ]]))
       
@@ -44,10 +48,10 @@ CalcMfx <- function(object, X, pred_fun = predict, predictors = colnames(X),
     # get predictions for each point in the sequence
     yhat <- lapply(pts, function(point){
       
-      X_new <- cbind(X[ , setdiff(names(X), p) ], rep(point, nrow(X)))
+      X_new <- X
       
-      colnames(X_new)[ ncol(X_new) ] <- p
-      
+      X_new[[ p ]] <- point
+
       yhat <- pred_fun(object, X_new)
       
       yhat
@@ -59,21 +63,63 @@ CalcMfx <- function(object, X, pred_fun = predict, predictors = colnames(X),
     yh0 <- yhat[ 1 , ]
     
     # get the derivative
-    dy <- apply(yhat, 2, function(y) c(NA, diff(y)))
-    
-    dx <- c(NA, diff(pts))
-    
-    # get the mfx
-    dydx <- dy / dx
-    
-    mfx <- mean(dydx, na.rm = TRUE)
-    
-    stdd <- sd(dydx, na.rm = TRUE)
-    
-    # confidence interval (totally overconfident, does not account for non-linearity)
-    conf <- c(mfx - 1.96 * stdd / sqrt(length(dydx)),
-              mfx + 1.96 * stdd / sqrt(length(dydx)))
-    
+    if (is.factor(X[[ p ]])) {
+      
+      warning("Factor detected. CalcMfx for factors is in prototype. 
+              I have reason to believe it doesn't calculate the right thing.
+              Caveat emptor!")
+      
+      ### More research is needed to get factors right...
+      # since each level of p represents a binary variable, I think I can just
+      # take rowMeans of yhat to get the marginal effect. But I need to do more work
+      
+      # dy <- yhat
+      
+      dy <- apply(yhat, 2, function(y){
+        ans <- numeric(length(y))
+        
+        for (j in seq_along(ans)) {
+          ans[ j ] <- y[ j ] - mean(y[ -j ])
+        }
+        ans
+      })
+      
+      dx <- rep(1, length(pts))
+      
+      dydx <- dy
+      
+      mfx <- rowMeans(dydx, na.rm = TRUE)
+      
+      stdd <- apply(dydx, 1, function(y) sd(y, na.rm = TRUE))
+      
+      # confidence interval (totally overconfident, does not account for non-linearity)
+      conf <- cbind(mfx - 1.96 * stdd / sqrt(ncol(dydx)),
+                    mfx + 1.96 * stdd / sqrt(ncol(dydx)))
+      
+      names(mfx) <- levels(X[[ p ]])
+      rownames(conf) <- names(mfx)
+      
+      is_factor <- TRUE
+
+    } else {
+      dy <- apply(yhat, 2, function(y) c(NA, diff(y)))
+      
+      dx <- c(NA, diff(pts))
+      
+      dydx <- dy / dx
+      
+      # get the mfx
+      mfx <- mean(dydx, na.rm = TRUE)
+      
+      stdd <- sd(dydx, na.rm = TRUE)
+      
+      # confidence interval (totally overconfident, does not account for non-linearity)
+      conf <- c(mfx - 1.96 * stdd / sqrt(length(dydx)),
+                mfx + 1.96 * stdd / sqrt(length(dydx)))
+      
+      is_factor <- FALSE
+    }
+  
     
     # return the result
     result <- list(mfx = mfx, conf = conf,
@@ -81,7 +127,8 @@ CalcMfx <- function(object, X, pred_fun = predict, predictors = colnames(X),
                    dx = dx,
                    x = pts,
                    yh0 = yh0,
-                   varname = p)
+                   varname = p,
+                   is_factor = is_factor)
     
     class(result) <- "Mfx"
     
@@ -108,21 +155,81 @@ plot.Mfx_list <- function(mfx_list, ...) {
 }
 
 
-plot.Mfx <- function(mfx, type = c("derivative", "level"), centered = TRUE, ...){
+plot.Mfx <- function(mfx, type = c("response", "derivative"), centered = FALSE, ...){
 
   ### Below still only works with one variable
   
   ### check consistency of inputs ----
   # TODO
   
+  ### Declare a function to get our plot options in order ----
+  DoDotsProcedure <- function(...){
+    
+    dots <- list(...)
+    
+    if ( ! "ylim" %in% names(dots)) {
+      dots$ylim <- range(plotmat, na.rm = TRUE)
+    }
+    
+    if (! "col" %in% names(dots)) {
+      dots$col <- rgb(0,0,0,0.15)
+    }
+    
+    if (! "ylab" %in% names(dots)) {
+      dots$ylab <- ylab
+    }
+    
+    if (! "xlab" %in% names(dots)) {
+      dots$xlab <- mfx$varname
+    }
+    
+    if ("type" %in% names(dots)) {
+      warning("reverting type to type = 'l'")
+    }
+    
+    dots$type <- "l"
+    
+    dots
+  }
+  
+  ### If we have a factor variable, do its plot and exit early ----
+  if (mfx$is_factor) {
+    
+    if (type[ 1 ] != "response") {
+      message("Derivative not meaningful for factor variables")
+    }
+    
+    plotmat <- mfx$dy # which is actually just yhat
+    
+    ylab <- "partial y-hat"
+    
+    dots <- DoDotsProcedure(...) # for now, I am ignoring this
+    
+    dots$x <- seq_along(mfx$mfx)
+    
+    barcenters <- barplot(mfx$mfx, ylim = range(plotmat), 
+                          main = mfx$varname, ylab = "partial y-hat",
+                          col = "white", border = "white")
+    
+    for(j in 1:ncol(plotmat)){
+      lines(barcenters, plotmat[ , j ], col = rgb(0,0,0,0.15), type = "o", pch = 19)
+    } 
+    
+    lines(barcenters, rowMeans(plotmat), col = "blue", lwd = 3, lty = 2)
+    
+    points(barcenters, mfx$mfx, pch = 19, col = "red")
+    
+    return()
+  }
+  
   ### Do calculations to get the chosen plot ----
-  if (type[ 1 ] == "level") {
+  if (type[ 1 ] == "response") {
     plotmat <- rbind(mfx$yh0, mfx$dy[ -1 , ])
     
     plotmat <- apply(plotmat, 2, cumsum)
     
     mfx_pred <- mfx$x * mfx$mfx + 
-      mean(plotmat[ which.min(abs(mfx$x)) , ], na.rm = TRUE) # I'm not sure about this constant
+      mean(plotmat[ which.min(abs(mfx$x)) , ], na.rm = TRUE) 
     
     ylab <- "partial y-hat"
     
@@ -146,30 +253,8 @@ plot.Mfx <- function(mfx, type = c("derivative", "level"), centered = TRUE, ...)
   }
   
   # get our plot options straight
-  dots <- list(...)
-  
-  if ( ! "ylim" %in% names(dots)) {
-    dots$ylim <- range(plotmat, na.rm = TRUE)
-  }
-  
-  if (! "col" %in% names(dots)) {
-    dots$col <- rgb(0,0,0,0.3)
-  }
-  
-  if (! "ylab" %in% names(dots)) {
-    dots$ylab <- ylab
-  }
-  
-  if (! "xlab" %in% names(dots)) {
-    dots$xlab <- mfx$varname
-  }
-  
-  if ("type" %in% names(dots)) {
-    warning("reverting type to type = 'l'")
-  }
-  
-  dots$type <- "l"
-  
+  dots <- DoDotsProcedure(...)
+    
   ### do the plotting ----
   
   # plot the ICE curves
@@ -203,8 +288,11 @@ plot.Mfx <- function(mfx, type = c("derivative", "level"), centered = TRUE, ...)
 }
 
 summary.Mfx_list <- function(mfx_list){
-  tab <- data.frame(variable = names(mfx_list),
-                    effect = sapply(mfx_list, function(x) x$mfx))
+  tab <- do.call(rbind, lapply(mfx_list, function(x){
+    data.frame(variable = paste(rep(x$varname, length(x$mfx)), names(x$mfx)),
+              effect = x$mfx,
+              stringsAsFactors = FALSE)
+  }))
   
   conf <- do.call(rbind, lapply(mfx_list, function(x) x$conf))
   
