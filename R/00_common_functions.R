92fa37e6dfd63284cfdc01ec2d4ec78483155abf
Tommy Jones <jones.thos.w@gmail.com>
2017-11-09 21:40:52 -0500

    Add some helper functions

--- R/00_common_functions.R
+++ R/00_common_functions.R
@@ -380,3 +380,117 @@ CalcClassificationStats <- function(predicted_probabilities, true_values){
 }
 
 
+#' Partition a dataset into K folds for cross-validation
+#' @description This function takes a dataset as input and returns a list with
+#'  indices for observations in each fold and the dataset itself.
+#'
+#' @param data A matrix or data frame of the data to be partitioned
+#' @param k An iteger indicating the number of folds
+#' @param strat_var A character or numeric indexing the column of 
+#'  \code{data} on which to stratify. Note that \code{data[[ strat_var ]]} must
+#'  be categorical. i.e. It must be a character or factor.
+#' @param seed An integer to set the random seed if desired. Defaults to \code{NULL}.
+#'
+#' @return
+#' Returns a \code{list} of length K. Each element of this list has
+#' indices for training and testing at each fold. For example, \code{training}
+#' contains indices for K - 1 folds for training a model and \code{test} contains
+#' indices for the K-th fold for testing.
+#' @export
+#' @examples
+#' data(mtcars)
+#'
+#' folds <- CreateKFolds(data = mtcars, k = 10)
+SampleKFolds <- function(data, k, strat_var = NULL, seed = NULL) {
+  
+  # set seed if desired / not already set
+  if( ! is.null(seed) ) set.seed(seed)
+  
+  # if data doesn't have rownames, give it to them
+  if (is.null(rownames(data))) {
+    rownames(data) <- 1:nrow(data)
+  }
+  
+  # declare a cv function
+  MakeCv <- function(data, k) {
+    # set up basics
+    rows <- rownames(data)
+    
+    size <- floor(nrow(data)/k)
+    
+    remainder <- nrow(data) - size * k # make each fold as balanced as possible
+    
+    # get the indexes of each fold into a list
+    folds <- vector(mode="list", length=k)
+    
+    for(j in 1:k){
+      folds[[ j ]] <- sample(rows, size=size, replace=FALSE)
+      rows <- rows[ ! rows %in% folds[[ j ]] ]
+    }
+    
+    if( remainder > 0){
+      for(j in 1:length(rows)){
+        folds[[ j ]][ length(folds[[ j ]]) + 1 ] <- sample(rows, size=1)
+        rows <- rows[ ! rows %in% folds[[ j ]] ]
+      }
+    }
+    
+    # combinations of each fold to use for training sets / test sets
+    combos <- combn(x=1:k, m=(k-1))
+    
+    # pull indices into format that's easier to use
+    cv.folds <- vector(mode="list", length=k)
+    
+    for( j in 1:k ){
+      cv.folds[[ j ]]$training <- unlist( folds[ combos[ , j ] ] )
+      cv.folds[[ j ]]$test <- folds[[ (1:k)[ ! (1:k) %in% combos[ , j ] ]  ]]
+    }
+    
+    result <- cv.folds
+    return(result)
+  }
+  
+  # Check whether or not we're stratifying. If yes, do it. If no, just do CV on whole.
+  if ( ! is.null(strat_var) ) {
+    
+    # is the input correctly formatted?
+    if ( ! class(data[[ strat_var ]]) %in% c("factor", "character"))
+      stop("class(data[[ strat_var ]] must be one of factor or character to stratify")
+    
+    # stratify then sample and re-combine
+    # Note: this could be WAY more efficient, I'm sure
+    result <- by(data, INDICES = data[[ strat_var ]], function(x){
+      MakeCv(data = x, k = k)
+    })
+    
+    result <- lapply(seq_len(k), function(fold){
+      
+      training <- unlist(lapply(result, function(strat){
+        strat[[ fold ]]$training
+      }))
+      
+      test <- unlist(lapply(result, function(strat){
+        strat[[ fold ]]$test
+      }))
+      
+      list(training = training, test = test)
+    })
+  } else {
+    result <- MakeCv(data = data, k = k)
+  }
+  
+  return(result)
+  
+}
+
+Factor2Binary <- function(y){
+  ynames <- levels(y)
+  
+  Y <- data.frame(y = y)
+  
+  Y <- model.matrix(~y, Y)
+  
+  colnames(Y) <- ynames
+  
+  Y
+}
